62b882c34b1003658f14acaade31bd82f531205a
oilimee645 <oilimee645@gmail.com>
2023-02-01 00:22:10 -0600

    Add sdk 19

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,7 +1,10 @@
 public class HolaMundo {
 
     public static void main(String[] args) {
+
         System.out.println("HOLA EMG \nJAVA TUTO");
+
+        System.out.println("Nuevo salto de linea");
     }
 
 }
